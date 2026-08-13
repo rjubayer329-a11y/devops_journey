464e4883809ddbd3d8672d68075804ed3f5c4914
Jubayer Rahman <rjubayer329@gmail.com>
2026-08-13 16:26:59 +0800

    Add health chack script to get response from server

---
+++ health_check.py
@@ -0,0 +1,25 @@
+import urllib.request
+import urllib.error
+import time
+
+endpoints = [
+    "https://httpbin.org/status/200",
+    "https://httpbin.org/status/500",
+    "https://httpbin.org/delay/1"
+]
+
+
+for link in endpoints:
+    start_time = time.time()
+    try:
+        with urllib.request.urlopen(link, timeout=5) as response:
+            end_time = time.time()
+            duration = round((end_time - start_time)*1000, 2)
+            print(
+                f"✅ [UP] {link} - Status: {response.status} OK - Latency:"
+                f"{duration}ms"
+            )
+    except urllib.error.HTTPError as e:
+        print(f"❌ [DOWN] {link} - HTTP Error: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"⚠️ [FAILED] {link} - Connection Error: {e.reason}")
